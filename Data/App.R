f0ba5d8685cf240140a7ee8c3ebda6b9fd975661
fynngeh <fynngeh@gmail.com>
2026-01-28 20:13:54 -0700

    finished

--- Data/App.R
+++ Data/App.R
@@ -57,13 +57,10 @@ server <- function(input, output, session) {
 
   output$pitch <- renderPlot({
     ggplot() +
-      # Spielfeld Rand
       geom_rect(aes(xmin = 0, xmax = 120, ymin = 0, ymax = 80),
                 fill = "#2E8B57", color = "black", linewidth = 1) +
-      # Mittellinie
       geom_segment(aes(x = 60, xend = 60, y = 0, yend = 80),
                    color = "black", linewidth = 0.6) +
-      # Strafräume
       geom_rect(aes(xmin = 102, xmax = 120, ymin = 18, ymax = 62),
                 fill = NA, color = "black", linewidth = 0.6) +
       geom_rect(aes(xmin = 0, xmax = 18, ymin = 18, ymax = 62),
